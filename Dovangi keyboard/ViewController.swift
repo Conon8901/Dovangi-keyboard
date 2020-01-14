214107fdd954baca41bca0b0760f1254ae44800b
黒岩修 <shu9618@gmail.com>
2020-01-14 19:38:02 +0900

    Initial Commit

---
+++ Dovangi keyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dovangi keyboard
+//
+//  Created by 黒岩修 on 1/14/2 R.
+//  Copyright © 2 Reiwa 黒岩修. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
